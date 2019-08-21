2fc94a413743ae3d4432978a70d4532574eb25df
Martin Milius <martin.milius@gmail.com>
2019-08-21 02:48:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
     value.each do |description, array|
       array.each do |name|
         if pigeon_list[name] == nil
-          pigeon_list[name].has_key(key)
+          pigeon_list[name] = {}
 end
 if pigeon_list[name][attribute] == nil
   pigeon_list[name][attribute] = []
